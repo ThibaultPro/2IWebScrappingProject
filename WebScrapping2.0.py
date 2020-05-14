53b6aa62e87d3fe0279ea9626206622de760f641
Tpierga <thibaultpierga@outlook.fr>
2020-05-14 19:23:09 +0200

    first and last commit with linux compatibility and pictures

---
+++ WebScrapping2.0.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  1 21:57:01 2020
+
+@author: thiba
+"""
+from Application import *
+
+"""
+This is the main file
+Run it in your python IDE to see the app displayed
+"""
+
+app = App()
+app.mainloop()
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  1 21:57:01 2020
+
+@author: thiba
+"""
+from Application import *
+
+"""
+This is the main file
+Run it in your python IDE to see the app displayed
+"""
+if __name__ == '__main__':
+    app = App()
+    app.mainloop()
+
+
+
+
+
+
